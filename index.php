b93b1a3fb9d13dee1a1a5ed58af45923516c489b
lukyanov955 <140232190+lukyanov955@users.noreply.github.com>
2024-06-28 10:15:44 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>3.0 Страница приветствия</title>
+    <title>3.1 Страница приветствия</title>
     <style>
         body {
             font-family: Arial, sans-serif;
