e3569f75fe338c29bd607fce6d4ef8684dcb151e
lukyanov955 <lukyanov955@yandex.kz>
2024-06-28 10:08:52 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>2,1,Страница приветствия</title>
+    <title>2,1,С2траница приветствия</title>
     <style>
         body {
             font-family: Arial, sans-serif;
